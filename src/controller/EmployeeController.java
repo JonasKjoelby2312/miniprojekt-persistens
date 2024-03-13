7c6251eef142b0ff6d55dc743d3a95001c888f56
ChristianBjerreSkadhauge <10503126@ucn.dk>
2024-03-13 13:40:28 +0100

    EmployeeController Changes

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -1,13 +1,31 @@
-package controller;
+	package controller;
 
-import db.EmployeeDAO;
+	import db.EmployeeDAO;
+	import db.EmployeeDB;
+	import model.Employee;
+	import java.util.List;
 
-public class EmployeeController {
-	
-	private EmployeeDAO employeeDB;
-	
-	public EmployeeController() {
-		this.employeeDB = new EmployeeDB();
+
+	public class EmployeeController {
+		
+		private EmployeeDAO employeeDB;
+		
+		public EmployeeController() throws Exception {
+			this.employeeDB = new EmployeeDB();
+			
+		}
+		
+		public List<Employee> findAllEmployees() throws Exception {
+			return employeeDB.findAll();
+		}
+
+		public Employee findByID(int id) throws Exception {
+			return employeeDB.findById(id);
+		}
+		
+		public void updateEmployee(Employee e) throws Exception {
+			employeeDB.updateEmployee(e);
+		}
 	}
 
-}
+
